b1de0b2be350e07e96bb88061e1b3ff764e152f3
thientran0801 <thientranbk98@gmail.com>
2023-11-05 23:59:00 +0700

    done session 04 module 04

---
+++ ss04/bai_tap/bt1.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE QuanLySinhVien;
+USE QuanLySinhVien;
+CREATE TABLE Class
+(
+    ClassID   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ClassName VARCHAR(60) NOT NULL,
+    StartDate DATETIME    NOT NULL,
+    Status    BIT
+);
+CREATE TABLE Student
+(
+    StudentId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    StudentName VARCHAR(30) NOT NULL,
+    Address     VARCHAR(50),
+    Phone       VARCHAR(20),
+    Status      BIT,
+    ClassId     INT         NOT NULL,
+    FOREIGN KEY (ClassId) REFERENCES Class (ClassID)
+);
+CREATE TABLE Subject
+(
+    SubId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubName VARCHAR(30) NOT NULL,
+    Credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
+    Status  BIT                  DEFAULT 1
+);
+
+CREATE TABLE Mark
+(
+    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubId     INT NOT NULL,
+    StudentId INT NOT NULL,
+    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubId, StudentId),
+    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
+    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+);
+
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+select * from `subject`
+where `subject`.credit in (
+select max(credit) from `subject`
+);
+
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select `subject`.*, mark.mark as top_mark
+from `subject` join mark on `subject`.subid = mark.subid
+having top_mark >= all (select mark from mark);
+
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+select s.*, avg(m.mark) as average
+from student s join mark m on s.StudentId = m.StudentId
+group by s.StudentId, s.StudentName
+order by average desc;
