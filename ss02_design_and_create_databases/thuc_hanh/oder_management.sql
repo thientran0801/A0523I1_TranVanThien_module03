f9ed32cbc00b5fe0256f33d0e10e9f5e6a62f1d3
thientran0801 <thientranbk98@gmail.com>
2023-11-05 21:32:11 +0700

    done session 03 module 03

---
+++ ss02_design_and_create_databases/thuc_hanh/oder_management.sql
@@ -0,0 +1,37 @@
+create database order_management;
+use order_management;
+
+
+-- - Thực thể ĐƠN VỊ ĐH gồm các thuộc tính sau:
+
+-- Mã ĐV
+-- Tên ĐV
+-- Địa chỉ
+-- Điện thoại
+-- - Thực thể ĐƠN VỊ KH gồm các thuộc tính sau:
+
+-- Mã ĐV
+-- Tên ĐV
+-- Địa chỉ
+-- - Thực thể HÀNG gồm các thuộc tính sau:
+
+-- Mã hàng
+-- Tên hàng
+-- Đv tính
+-- Mô tả hàng
+-- - Thực thể NGƯỜI ĐẶT gồm các thuộc tính sau:
+
+-- Mã số NĐ
+-- Họ tên NĐ
+-- - Thực thể NƠI GIAO gồm các thuộc tính sau:
+
+-- Mã số ĐĐG
+-- Tên nơi giao
+-- - Thực thể NGƯỜI NHẬN gồm các thuộc tính sau:
+
+-- Mã số NN
+-- Họ tên NN
+-- - Thực thể NGƯỜI GIAO gồm các thuộc tính sau:
+
+-- Mã số NG
+-- Họ tên NG
--- ss03_manipulate_the_database/bai_tap/bt2.sql
+++ ss03_manipulate_the_database/bai_tap/bt2.sql
@@ -30,3 +30,67 @@ primary key( p_id, o_id),
 foreign key(p_id) references product(p_id),
 foreign key(o_id) references `order`(o_id)
 );
+alter table oder_detail
+rename to  order_detail;
+alter table order_detail
+drop  od_id; 
+
+insert into customer(c_name, c_age)
+value('Minh Quan', 10),
+('Ngoc Oanh',20),
+('Hong Ha', 50);
+
+insert into `order`(c_id, o_date)
+value(1,'2006-03-21'),
+(2,'2006-03-23'),
+(1,'2006-03-16');
+
+insert into product(p_name, p_price)
+value('May Giat', 3),
+('Tu Lanh',5),
+('Dieu Hoa',7),
+('Quat',1),
+('Bep Dien', 2);
+
+insert into order_detail(o_id, p_id, od_qty) 
+values(1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+
+
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+
+select o_id, o_date, o_totalprice
+from `order`;
+
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+
+select c.c_name, p.p_name
+from customer c join `order` o on c.c_id = o.c_id 
+join order_detail od on o.o_id = od.o_id
+join product p on p.p_id = od.p_id;
+
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+
+select c.c_name
+from customer c left join `order` o on c.c_id = o.c_id
+where o.c_id is null;
+
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
+-- (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. 
+-- Giá bán của từng loại được tính = odQTY*pPrice)
+
+select o.o_id, o.o_date, sum(p.p_price*od_qty) as 'price_total'
+from `order` o join order_detail od on o.o_id = od.o_id
+join product p on p.p_id = od.p_id
+group by o.o_id;
+
+
+
+
+
+
